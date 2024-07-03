9786deadb063e19c6d77531cd645307ec3e54f46
max <maxtor3569@gmail.com>
2024-07-04 06:36:45 +0900

    fix

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -20,6 +20,8 @@ import { getSession, commitSession } from '~/session.server';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
+  console.log(request);
+  
   const shopId = url.searchParams.get("shopId");
   if (!shopId) {
     return json({ error: "Shop ID is required" }, { status: 400 });
