1b7e19bb2a1eb2cd53b5a572ce2479d4e8f072a8
max <maxtor3569@gmail.com>
2024-07-16 00:27:48 +0200

    fix

--- app/routes/api.forceSession.tsx
+++ app/routes/api.forceSession.tsx
@@ -1,8 +1,24 @@
 import {getSession} from "~/session.server";
 import {json} from "@remix-run/node";
 import {authenticate} from "../shopify.server";
+import prisma from "~/db.server";
 export async function loader({ request }) {
     const {admin, session} = await authenticate.admin(request);
     console.log(admin);
     console.log(session);
+
+    await prisma.session.create({
+        data: {
+            id: session.id,
+            shop: session.shop,
+            state: session.state,
+            isOnline: session.isOnline,
+            scope: session.scope,
+            expires: session.expires ? new Date(session.expires) : null,
+            accessToken: session.accessToken,
+            userId: session.userId ? BigInt(session.userId) : null,
+        },
+    });
+
+    return json({ accessToken: session.accessToken});
 }
